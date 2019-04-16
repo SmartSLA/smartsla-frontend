abbaa72085a1fab123622dd4a6142f00de3cf57c
kferjani <kferjani@linagora.com>
2019-04-16 17:36:36 +0100

    removed extra element from side bar

--- src/components/request/IssueDetailSideBar.vue
+++ src/components/request/IssueDetailSideBar.vue
@@ -2,23 +2,6 @@
   <v-container class="pt-0 px-0">
     <v-layout row wrap>
       <v-flex xs12>
-        <v-list three-line subheader>
-          <v-subheader inset class="text-uppercase blue text-md-center white--text ml-0">{{
-            $t("interlocutor in charge of the request")
-          }}</v-subheader>
-          <v-card class="pt-2">
-            <v-icon large color="blue" class="arrow-down pr-5 pt-2">play_arrow</v-icon>
-            <br />
-            <v-img class="ml-5 mr-5" :src="avatarUrl" height="150px"></v-img>
-            <v-card-title primary-title>
-              <div>
-                <div class="title text-xs-center ml-4 grey--text text-capitalize">{{ displayName }}</div>
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-list>
-      </v-flex>
-      <v-flex xs12>
         <side-bar-links />
       </v-flex>
     </v-layout>
