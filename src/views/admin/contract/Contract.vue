115294262394507b34bf82d735721a8eae232cb5
wajdi gharbi <wgharbi@linagora.com>
2019-06-24 09:59:55 +0200

    #97

--- src/views/admin/contract/Contract.vue
+++ src/views/admin/contract/Contract.vue
@@ -40,9 +40,7 @@
                   <v-flex xs4>
                     <div class="subheading font-weight-medium">{{ $t("Client") }} :</div>
                   </v-flex>
-                  <v-flex xs8>
-                    {{ contract.client }}
-                  </v-flex>
+                  <v-flex xs8>{{ contract.client }}</v-flex>
                   <v-flex xs4>
                     <div class="subheading font-weight-medium">{{ $t("Name") }} :</div>
                   </v-flex>
@@ -80,9 +78,9 @@
                   </v-flex>
                   <v-flex xs8>{{ $t(contract.status) ? $t("active") : $t("not active") }}</v-flex>
                   <v-flex xs4>
-                    <div class="subheading font-weight-medium">
-                      {{ $t("requests shared among the beneficiaries") }} :
-                    </div>
+                    <div
+                      class="subheading font-weight-medium"
+                    >{{ $t("requests shared among the beneficiaries") }} :</div>
                   </v-flex>
                   <v-flex xs8>{{ contract.sharedRequests ? $t("yes") : $t("no") }}</v-flex>
                 </v-layout>
@@ -135,19 +133,16 @@
                       v-if="props.item.SupportDate.start.length && props.item.SupportDate.start.length"
                     >
                       {{ $t("S") }}: {{ props.item.SupportDate.start }}
-                      <br />
+                      <br>
                       {{ $t("E") }}: {{ props.item.SupportDate.end }}
                     </td>
-                    <td v-else class="text-xs-center">
-                      {{ $t("contract in progress") }}
-                    </td>
+                    <td v-else class="text-xs-center">{{ $t("contract in progress") }}</td>
                     <td class="text-xs-center">
                       <v-chip
                         :color="critColor(props.item.critical)"
                         :text-color="critTextColor(props.item.critical)"
                         label
-                        >{{ $t(props.item.critical) }}</v-chip
-                      >
+                      >{{ $t(props.item.critical) }}</v-chip>
                     </td>
                     <td class="text-xs-center">
                       <span v-if="props.item.generic == 'yes'">{{ $t(props.item.generic) }}</span>
@@ -159,23 +154,69 @@
               </v-card-text>
             </v-card>
           </v-flex>
-          <v-flex xs12>
+          
+        </v-layout>
+      </v-flex>
+      <v-flex xs5 pt-0>
+        <v-card>
+          <v-card-title primary-title>
+            <v-layout>
+              <v-flex xs6>
+                <h3 class="headline">{{ $t("Human resources") }}</h3>
+              </v-flex>
+              <v-flex xs6>
+                <div class="text-xs-center grey--text">
+                  <v-btn
+                    color="primary"
+                    fab
+                    small
+                    dark
+                    :to="{ name: 'Edit Contract', params: { id: contract._id, section: 'hr', type: 'hr' } }"
+                  >
+                    <v-icon>edit</v-icon>
+                  </v-btn>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-card-title>
+          <v-card-text>
+            <div class="subheading font-weight-regular pb-2">{{ $t("beneficiaries") }} :</div>
+            <ul class="pb-2 grey--text">
+              <li
+                v-for="beneficiary in contract.humanResources.beneficiaries"
+                :key="beneficiary.id"
+              >
+                <router-link to="#">{{ beneficiary.name }}</router-link>
+              </li>
+            </ul>
+            <v-divider class="ml-1 mr-1 pb-2"></v-divider>
+            <div class="subheading font-weight-regular">{{ $t("Teams") }} :</div>
+            <ul ul class="pb-2 grey--text">
+              <li v-for="team in contract.humanResources.teams" :key="team.id">
+                <router-link to="#">{{ team.name }}</router-link>
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+        <v-flex xs12>
             <v-card class="contractual-commitments">
               <v-card-title primary-title>
                 <v-layout>
                   <v-flex xs9>
                     <h4 class="headline">
                       {{ $t("Contractual commitments") }}
-                      <v-chip :color="critColor('critical')" :text-color="critTextColor('critical')" label>
-                        {{ $t("critical") }} </v-chip
-                      >:
+                      <v-chip
+                        :color="critColor('critical')"
+                        :text-color="critTextColor('critical')"
+                        label
+                      >{{ $t("critical") }}</v-chip>:
                       <span v-if="contract.Engagements.critical.schedule">
                         {{
-                          contract.Engagements.critical.schedule.end == "-"
-                            ? $t("7d/7")
-                            : `${contract.Engagements.critical.schedule.start}H - ${
-                                contract.Engagements.critical.schedule.end
-                              }H`
+                        contract.Engagements.critical.schedule.end == "-"
+                        ? $t("7d/7")
+                        : `${contract.Engagements.critical.schedule.start}H - ${
+                        contract.Engagements.critical.schedule.end
+                        }H`
                         }}
                       </span>
                     </h4>
@@ -224,16 +265,18 @@
                   <v-flex xs9>
                     <h4 class="headline">
                       {{ $t("Contractual commitments") }}
-                      <v-chip :color="critColor('sensible')" :text-color="critTextColor('sensible')" label>
-                        {{ $t("sensible") }} </v-chip
-                      >:
+                      <v-chip
+                        :color="critColor('sensible')"
+                        :text-color="critTextColor('sensible')"
+                        label
+                      >{{ $t("sensible") }}</v-chip>:
                       <span v-if="contract.Engagements.sensible.schedule">
                         {{
-                          contract.Engagements.sensible.schedule.end == "-"
-                            ? $t("7d/7")
-                            : `${contract.Engagements.sensible.schedule.start}H - ${
-                                contract.Engagements.sensible.schedule.end
-                              }H`
+                        contract.Engagements.sensible.schedule.end == "-"
+                        ? $t("7d/7")
+                        : `${contract.Engagements.sensible.schedule.start}H - ${
+                        contract.Engagements.sensible.schedule.end
+                        }H`
                         }}
                       </span>
                     </h4>
@@ -282,16 +325,18 @@
                   <v-flex xs9>
                     <h4 class="headline">
                       {{ $t("Contractual commitments") }}
-                      <v-chip :color="critColor('standard')" :text-color="critTextColor('standard')" label>
-                        {{ $t("standard") }} </v-chip
-                      >:
+                      <v-chip
+                        :color="critColor('standard')"
+                        :text-color="critTextColor('standard')"
+                        label
+                      >{{ $t("standard") }}</v-chip>:
                       <span v-if="contract.Engagements.standard.schedule">
                         {{
-                          contract.Engagements.standard.schedule.end == "-"
-                            ? $t("7d/7")
-                            : `${contract.Engagements.standard.schedule.start}H - ${
-                                contract.Engagements.standard.schedule.end
-                              }H`
+                        contract.Engagements.standard.schedule.end == "-"
+                        ? $t("7d/7")
+                        : `${contract.Engagements.standard.schedule.start}H - ${
+                        contract.Engagements.standard.schedule.end
+                        }H`
                         }}
                       </span>
                     </h4>
@@ -333,47 +378,8 @@
               </v-card-text>
             </v-card>
           </v-flex>
-        </v-layout>
       </v-flex>
-      <v-flex xs5 pt-0>
-        <v-card>
-          <v-card-title primary-title>
-            <v-layout>
-              <v-flex xs6>
-                <h3 class="headline">{{ $t("Human resources") }}</h3>
-              </v-flex>
-              <v-flex xs6>
-                <div class="text-xs-center grey--text">
-                  <v-btn
-                    color="primary"
-                    fab
-                    small
-                    dark
-                    :to="{ name: 'Edit Contract', params: { id: contract._id, section: 'hr', type: 'hr' } }"
-                  >
-                    <v-icon>edit</v-icon>
-                  </v-btn>
-                </div>
-              </v-flex>
-            </v-layout>
-          </v-card-title>
-          <v-card-text>
-            <div class="subheading font-weight-regular pb-2">{{ $t("beneficiaries") }} :</div>
-            <ul class="pb-2 grey--text">
-              <li v-for="beneficiary in contract.humanResources.beneficiaries" :key="beneficiary.id">
-                <router-link to="#">{{ beneficiary.name }}</router-link>
-              </li>
-            </ul>
-            <v-divider class="ml-1 mr-1 pb-2"></v-divider>
-            <div class="subheading font-weight-regular">{{ $t("Teams") }} :</div>
-            <ul ul class="pb-2 grey--text">
-              <li v-for="team in contract.humanResources.teams" :key="team.id">
-                <router-link to="#">{{ team.name }}</router-link>
-              </li>
-            </ul>
-          </v-card-text>
-        </v-card>
-      </v-flex>
+      
     </v-layout>
   </v-container>
 </template>
