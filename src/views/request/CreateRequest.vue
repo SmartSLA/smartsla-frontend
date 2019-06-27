11d6ff54259229aab3332b4d54f18ee6b8d5dcdb
wajdi gharbi <wgharbi@linagora.com>
2019-06-27 09:57:28 +0200

    #99

--- src/views/request/CreateRequest.vue
+++ src/views/request/CreateRequest.vue
@@ -57,19 +57,27 @@
                           background-color="white"
                           v-model="ticket.software"
                           :search-input.sync="software"
-                          :rules="[() => ticket.software.length > 0 || $i18n.t('Required field')]"
                           class="required-element"
                           return-object
+                          required
                         >
                           <template v-slot:item="data">
                             <v-chip label v-if="data.item.critical == 'critical'" color="red">C</v-chip>
-                            <v-chip label v-else-if="data.item.critical == 'sensible'" color="orange">S</v-chip>
+                            <v-chip
+                              label
+                              v-else-if="data.item.critical == 'sensible'"
+                              color="orange"
+                            >S</v-chip>
                             <v-chip label v-else color="grey">S</v-chip>
                             {{ data.item.name }} {{ data.item.version }} {{ data.item.os }}
                           </template>
                           <template v-slot:selection="data">
                             <v-chip label v-if="data.item.critical == 'critical'" color="red">C</v-chip>
-                            <v-chip label v-else-if="data.item.critical == 'sensible'" color="orange">S</v-chip>
+                            <v-chip
+                              label
+                              v-else-if="data.item.critical == 'sensible'"
+                              color="orange"
+                            >S</v-chip>
                             <v-chip label v-else color="grey">S</v-chip>
                             {{ data.item.name }} {{ data.item.version }} {{ data.item.os }}
                           </template>
@@ -106,7 +114,7 @@
                     {{ $t("ticket contractual engagements") }} : {{ $t("Supported in") }}
                     {{ $t(selectedEngagement.supported) }}, {{ $t("bypass in") }} {{ $t(selectedEngagement.bypassed) }},
                     {{ $t("and resolution in") }} {{ $t(selectedEngagement.fix) }}
-                    <br />
+                    <br>
                   </v-flex>
                   <v-flex xs12>
                     <v-input prepend-icon="notes">
@@ -142,7 +150,11 @@
                           class="pt-0"
                         >
                           <template v-slot:append-outer>
-                            <v-btn solo class="ml-0 white black--text mt-0 full-height" @click.native="addRelated">
+                            <v-btn
+                              solo
+                              class="ml-0 white black--text mt-0 full-height"
+                              @click.native="addRelated"
+                            >
                               <v-icon dark>add</v-icon>
                             </v-btn>
                           </template>
@@ -151,13 +163,16 @@
                       <v-flex xs2></v-flex>
                     </v-layout>
                     <div v-for="(link, key) in linkedRequests" :key="key" class="pl-4">
-                      <v-chip v-model="linkedRequests[key]" close>{{ link.link }} : {{ link.request }}</v-chip>
+                      <v-chip
+                        v-model="linkedRequests[key]"
+                        close
+                      >{{ link.link }} : {{ link.request }}</v-chip>
                     </div>
                   </v-flex>
                   <v-flex xs6></v-flex>
                   <v-flex xs6></v-flex>
                   <v-flex xs10 class="pl-4">
-                    <br />
+                    <br>
                     <file-upload
                       prepend-icon="attach_file"
                       class="file"
@@ -175,7 +190,11 @@
               <v-layout>
                 <v-flex xs6 text-xs-right align-end>
                   <v-spacer></v-spacer>
-                  <v-btn :disabled="submitRequest" :loading="submitRequest" @click="submit">{{ $t("Submit") }}</v-btn>
+                  <v-btn
+                    :disabled="submitRequest"
+                    :loading="submitRequest"
+                    @click="submit"
+                  >{{ $t("Submit") }}</v-btn>
                 </v-flex>
               </v-layout>
             </v-card-actions>
@@ -214,7 +233,15 @@ export default {
       },
       linkedRequest: "",
       linkType: "",
-      linkTypes: ["lié à", "duplique", "dupliqué par", "bloque", "bloqué par", "précède", "suit"],
+      linkTypes: [
+        "lié à",
+        "duplique",
+        "dupliqué par",
+        "bloque",
+        "bloqué par",
+        "précède",
+        "suit"
+      ],
       linkedRequests: [],
       submitRequest: false,
       states: ["Item 1", "Item 2", "Item 3", "Item 4"],
@@ -226,7 +253,14 @@ export default {
       softwareList: [],
       contractList: [],
       types: ["type1", "type2", "type3", "type4"],
-      relatedRequests: ["#1 issue1", "#3 issue3", "#18 issue18", "#41 issue41", "#35 issue35", "#70 issue70"],
+      relatedRequests: [
+        "#1 issue1",
+        "#3 issue3",
+        "#18 issue18",
+        "#41 issue41",
+        "#35 issue35",
+        "#70 issue70"
+      ],
       engagementsCategory: [],
       selectedTypes: []
     };
@@ -278,14 +312,17 @@ export default {
           if (this.ticket.software.critical) {
             switch (this.ticket.software.critical) {
               case "critical":
-                engagements = this.ticket.contract.Engagements.critical.engagements;
+                engagements = this.ticket.contract.Engagements.critical
+                  .engagements;
                 break;
               case "sensible":
-                engagements = this.ticket.contract.Engagements.sensible.engagements;
+                engagements = this.ticket.contract.Engagements.sensible
+                  .engagements;
 
                 break;
               case "standard":
-                engagements = this.ticket.contract.Engagements.standard.engagements;
+                engagements = this.ticket.contract.Engagements.standard
+                  .engagements;
 
                 break;
             }
@@ -302,7 +339,9 @@ export default {
     severityList() {
       var engagements = [...this.selectedTypes];
       var severities = [];
-      severities = engagements.filter(engagement => engagement.request == this.ticket.type).slice();
+      severities = engagements
+        .filter(engagement => engagement.request == this.ticket.type)
+        .slice();
       this.engagementsCategory = severities.slice();
       severities = severities.map(engagement => engagement.severity);
       return [...new Set(severities)];
@@ -311,7 +350,9 @@ export default {
       if (this.ticket.severity.length) {
         var engagements = [];
         engagements = this.engagementsCategory.filter(
-          engagement => engagement.request == this.ticket.type && (engagement.severity = this.ticket.severity)
+          engagement =>
+            engagement.request == this.ticket.type &&
+            (engagement.severity = this.ticket.severity)
         );
         return engagements[0];
       }
