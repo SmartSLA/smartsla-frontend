52b9039ceb1d9cbb1a6ebd895de4f86fab57d114
Christophe HAMERLING <chamerling@linagora.com>
2019-09-30 14:44:09 +0200

    #188 Paginate from REST API

--- src/store/modules/ticket.js
+++ src/store/modules/ticket.js
@@ -2,54 +2,74 @@ import Vue from "vue";
 
 function initialState() {
   return {
+    pagination: {
+      page: 1,
+      rowsPerPage: 10,
+      rowsPerPageItems: [10, 25, 50, 100]
+    },
     length: 0,
     tickets: {}
   };
 }
 
 const types = {
-  ADD_TICKET: "ADD_TICKET",
-  SET_TICKET_LENGTH: "SET_TICKET_LENGTH"
+  SET_TICKETS: "SET_TICKETS",
+  SET_TICKET_LENGTH: "SET_TICKET_LENGTH",
+  SET_PAGINATION: "SET_PAGINATION"
 };
 
 const actions = {
-  fetchTickets: ({ commit, dispatch }, options) => {
-    Vue.axios
-      .listTickets(options)
-      .then(response => {
-        (response.data || []).forEach(ticket => commit(types.ADD_TICKET, ticket));
-        dispatch("countTickets");
+  fetchTickets: ({ commit, dispatch, state }) => {
+    return Vue.axios
+      .listTickets({
+        limit: state.pagination.rowsPerPage,
+        offset: (state.pagination.page - 1) * state.pagination.rowsPerPage
       })
-      .catch(err => {
-        // TODO
-        console.log(err);
+      .then(response => {
+        dispatch("countTickets");
+        commit(types.SET_TICKETS, response.data);
       });
   },
 
   countTickets: ({ commit }) => {
-    Vue.axios
+    return Vue.axios
       .countTickets()
       .then(size => commit(types.SET_TICKET_LENGTH, size))
       .catch(err => {
         // TODO
         console.log(err);
       });
+  },
+
+  setPagination: ({ commit }, pagination) => {
+    commit(types.SET_PAGINATION, pagination);
   }
 };
 
 const mutations = {
-  [types.ADD_TICKET](state, ticket) {
-    Vue.set(state.tickets, ticket._id, ticket);
+  [types.SET_TICKETS](state, tickets) {
+    (tickets || []).forEach(ticket => Vue.set(state.tickets, ticket._id, ticket));
   },
 
   [types.SET_TICKET_LENGTH](state, length) {
     state.length = length;
+  },
+
+  [types.SET_PAGINATION](state, pagination) {
+    state.pagination = pagination;
   }
 };
 
 const getters = {
-  getNbOfTickets: state => state.length,
-  getTickets: state => Object.values(state.tickets) || []
+  getNbOfTickets: state => Number(state.length),
+  getTickets: state => Object.values(state.tickets) || [],
+  getCurrentPageRequests: state => {
+    return Object.values(state.tickets).slice(
+      (state.pagination.page - 1) * state.pagination.rowsPerPage,
+      state.pagination.page * state.pagination.rowsPerPage
+    );
+  },
+  pagination: state => state.pagination
 };
 
 export default {
