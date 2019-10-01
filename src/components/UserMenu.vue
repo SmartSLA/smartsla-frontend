a7bb7fd8fc123ff395824cbc14e212a68ff28b14
Rachid OUBRAIM <roubraim@linagora.com>
2019-10-01 16:24:18 +0200

    #180 Desactivate profile and settings in menu bar

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -16,13 +16,6 @@
                 </v-list-tile-content>
               </v-list-tile>
               <v-divider />
-              <v-list-tile @click.prevent="profile">
-                <v-list-tile-title>{{ $t("Profile") }}</v-list-tile-title>
-              </v-list-tile>
-              <v-divider />
-              <v-list-tile @click.prevent="settings">
-                <v-list-tile-title>{{ $t("Settings") }}</v-list-tile-title>
-              </v-list-tile>
               <v-list-tile @click.prevent="logout">
                 <v-list-tile-title>{{ $t("Logout") }}</v-list-tile-title>
               </v-list-tile>
