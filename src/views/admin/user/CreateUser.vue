17e0acbf6729d30443d560b8801faf763a2b0486
Christophe HAMERLING <chamerling@linagora.com>
2019-10-08 14:54:41 +0200

    Remove client/contract when creating user

--- src/views/admin/user/CreateUser.vue
+++ src/views/admin/user/CreateUser.vue
@@ -108,11 +108,11 @@
                   v-model="user.client"
                 ></v-select>
               </v-flex>
-              <v-flex xs1 v-if="user.type != 'expert'"></v-flex>
-              <v-flex xs3 class="pt-4">
+              <v-flex xs1></v-flex>
+              <v-flex xs3 class="pt-4" v-if="user.type !== 'expert'">
                 <span class="title">{{ $t("Contracts") }}</span>
               </v-flex>
-              <v-flex xs8>
+              <v-flex xs8 v-if="user.type !== 'expert'">
                 <v-select
                   :items="contractList"
                   item-value="_id"
@@ -121,9 +121,7 @@
                   v-model="user.contracts"
                 ></v-select>
               </v-flex>
-              <v-flex xs1></v-flex>
-              <v-flex xs5></v-flex>
-              <v-flex xs2>
+              <v-flex xs12 offset-xs5>
                 <v-btn class="success" @click="validateFrom">{{ $t("Create") }}</v-btn>
               </v-flex>
             </v-layout>
