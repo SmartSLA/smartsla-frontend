24537bfe80e9703054268b277e425603eabcf3ea
Rachid OUBRAIM <roubraim@linagora.com>
2019-11-07 17:02:32 +0100

    #369 Added roles menu item

--- src/components/admin/AdminMainSideBar.vue
+++ src/components/admin/AdminMainSideBar.vue
@@ -3,10 +3,7 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-list dark>
-          <v-list-group
-            value="true"
-            class="white--text"
-          >
+          <v-list-group value="true" class="white--text">
             <template v-slot:activator>
               <v-list-tile>
                 <v-list-tile-content>
@@ -15,36 +12,36 @@
               </v-list-tile>
             </template>
 
-            <v-list-tile
-              :to="{name: routeNames.USERS}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.USERS }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Users") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Users") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>people</v-icon>
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile
-              :to="{name: routeNames.ADMIN_ROLES}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.ADMIN_ROLES }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Roles") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Roles") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>verified_user</v-icon>
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile
-              :to="{name: routeNames.TEAMS}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.ADMIN_ROLES }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Teams") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Roles") }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon>verified_user</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+
+            <v-list-tile :to="{ name: routeNames.TEAMS }" active-class="primary">
+              <v-list-tile-content>
+                <v-list-tile-title class="pl-2">{{ $t("Teams") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>people_outline</v-icon>
@@ -52,10 +49,7 @@
             </v-list-tile>
           </v-list-group>
 
-
-          <v-list-group
-            value="true"
-          >
+          <v-list-group value="true">
             <template v-slot:activator>
               <v-list-tile>
                 <v-list-tile-content>
@@ -64,33 +58,25 @@
               </v-list-tile>
             </template>
 
-            <v-list-tile
-              :to="{name: routeNames.CLIENTS}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.CLIENTS }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Clients") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Clients") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>account_box</v-icon>
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile
-              :to="{name: routeNames.CONTRACTS}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.CONTRACTS }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Contracts") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Contracts") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>assignment</v-icon>
               </v-list-tile-action>
             </v-list-tile>
           </v-list-group>
-          <v-list-group
-            value="true"
-          >
+          <v-list-group value="true">
             <template v-slot:activator>
               <v-list-tile>
                 <v-list-tile-content>
@@ -99,24 +85,18 @@
               </v-list-tile>
             </template>
 
-            <v-list-tile
-              :to="{name: routeNames.SOFTWARELIST}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.SOFTWARELIST }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Software") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Software") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>web</v-icon>
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile
-              :to="{name: routeNames.ADMINCONTRIBUTIONS}"
-              active-class="primary"
-            >
+            <v-list-tile :to="{ name: routeNames.ADMINCONTRIBUTIONS }" active-class="primary">
               <v-list-tile-content>
-                <v-list-tile-title class="pl-2">{{ $t("Contributions") }}</v-list-tile-title>
+                <v-list-tile-title class="pl-2">{{ $t("Contributions") }}</v-list-tile-title>
               </v-list-tile-content>
               <v-list-tile-action>
                 <v-icon>library_add</v-icon>
@@ -140,4 +120,3 @@ export default {
   }
 };
 </script>
-
