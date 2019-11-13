e198c7df50c8d52e125a23ab88f5c2a577bb6944
Ismaeil Abouljamal <iabouljamal@linagora.com>
2019-11-13 15:59:48 +0100

    Make download-excel global and fetch all requests. See #190

--- src/main.js
+++ src/main.js
@@ -8,5 +8,7 @@ import "@/filters/calendarTimeFilter";
 import "@/filters/formatDateFilter";
 import "@/filters/striphtml";
 import "@/filters/prettyBytesFilter";
+import JsonExcel from "vue-json-excel";
 
 window.Application = getApplication(applicationInit(Vue));
+Vue.component("downloadExcel", JsonExcel);
