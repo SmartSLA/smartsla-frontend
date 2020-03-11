fdfc400c122b8186dca26edad0355ae06329c2b9
Khaled FERJANI <kferjani@linagora.com>
2020-03-11 11:08:26 +0100

    #237 updated login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,46 +1,48 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar class="login-bar">
-            <v-toolbar-title class="white--text login-header">{{ $t("OpenPaaS Login") }}</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form class="login-form" @keydown.native.enter="login">
-              <v-text-field
-                class="login-user-input"
-                prepend-icon="person"
-                name="login"
-                :label="$i18n.t('E-mail')"
-                type="text"
-                v-model="email"
-                autofocus
-              ></v-text-field>
-              <v-text-field
-                class="login-password-input"
-                prepend-icon="lock"
-                name="password"
-                :label="$i18n.t('Password')"
-                id="password"
-                type="password"
-                v-model="password"
-                required
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-layout justify-space-between>
-              <router-link :to="{ name: routeNames.RESET_PASSWORD }" target="_blank">
-                {{ $t("Reset Password") }}
-              </router-link>
-              <v-btn :disabled="logMeIn" :loading="logMeIn" @click="login">{{ $t("Login") }}</v-btn>
-            </v-layout>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-content id="login">
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm6 md5 lg4>
+          <v-card class="pa-3">
+            <v-toolbar class="login-bar">
+              <v-toolbar-title class="white--text login-header">{{ $t("OpenPaaS Login") }}</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form class="login-form" @keydown.native.enter="login">
+                <v-text-field
+                  class="login-user-input"
+                  prepend-icon="person"
+                  name="login"
+                  :label="$i18n.t('E-mail')"
+                  type="text"
+                  v-model="email"
+                  autofocus
+                ></v-text-field>
+                <v-text-field
+                  class="login-password-input"
+                  prepend-icon="lock"
+                  name="password"
+                  :label="$i18n.t('Password')"
+                  id="password"
+                  type="password"
+                  v-model="password"
+                  required
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-layout justify-space-between>
+                <router-link :to="{ name: routeNames.RESET_PASSWORD }" target="_blank">
+                  {{ $t("Reset Password") }}
+                </router-link>
+                <v-btn :disabled="logMeIn" :loading="logMeIn" @click="login">{{ $t("Login") }}</v-btn>
+              </v-layout>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
@@ -91,12 +93,11 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-button.v-btn.theme--light {
-  background-color: #2195f2 !important;
-  color: #fff;
-}
-
 .login-header {
   padding-left: 18px;
+}
+
+#login {
+  height: 60vh;
 }
 </style>
