3cea78994d47197e62127b90202f752f3edbb430
Stéphane VIEIRA <svieira@linagora.com>
2020-03-18 11:37:32 +0100

    #590 fix avatar width in navigation top navbar

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -7,7 +7,7 @@
       <v-layout row justify-end>
         <v-flex xs5>
           <v-menu offset-y class="pt-3 user-menu-item">
-            <op-avatar slot="activator" class="pr-3"></op-avatar>
+            <op-avatar slot="activator"></op-avatar>
             <v-list>
               <v-list-tile>
                 <v-list-tile-content>
