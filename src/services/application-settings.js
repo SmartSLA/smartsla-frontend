a03613ff8c8d9fdfff3ff9535d74a61f6bb673a6
Rachid OUBRAIM <roubraim@linagora.com>
2020-03-19 17:01:27 +0100

    #520 Added support account in the app settings

--- src/services/application-settings.js
+++ src/services/application-settings.js
@@ -2,7 +2,12 @@ export const ApplicationSettings = Object.freeze({
   ...{
     BASE_URL: "/",
     VUE_APP_OPENPAAS_URL: "http://localhost:8080",
-    SSP_URL: "http://ssp.localhost:8080/"
+    SSP_URL: "http://ssp.localhost:8080/",
+    SUPPORT_ACCOUNT: {
+      name: "08000Linux",
+      color: "#d32f2f",
+      logo: "logo.png"
+    }
   },
   ...process.env,
   ...window.openpaas
