019d24ea23584af4e55887fda6db07c5ed31c2ab
Stéphane VIEIRA <svieira@linagora.com>
2020-04-01 16:25:42 +0200

    #603 Remove the unused export button in request page

--- src/views/request/Request.vue
+++ src/views/request/Request.vue
@@ -6,9 +6,6 @@
           <button><v-icon>arrow_back</v-icon></button>
         </router-link>
       </div>
-      <div>
-        <v-btn flat small color="default"> <v-icon class="mr-2">backup</v-icon> {{ $t("Export sheet (CSV)") }} </v-btn>
-      </div>
     </v-layout>
     <v-layout row wrap justify-space-between>
       <v-flex xs12 md12 sm12 xl7 lg7 pl-3 pr-3 pt-4>
