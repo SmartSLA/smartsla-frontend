6c1e8be79757a0e8847096f5f2c1beb596c62137
Khaled FERJANI <kferjani@linagora.com>
2020-04-29 20:10:30 +0200

    #649 fixed client list filter view

--- src/views/admin/client/Clients.vue
+++ src/views/admin/client/Clients.vue
@@ -1,54 +1,61 @@
 <template>
-  <v-container class="pa-0" v-if="$auth.ready() && $auth.check('admin')">
-    <div class="page-title">
-      <span>{{ $t("Clients list") }}</span>
+  <v-container v-if="$auth.ready() && $auth.check('admin')">
+    <div class="clients-list">
+      <div class="page-title">
+        <span>{{ $t("Clients list") }}</span>
+      </div>
+      <v-layout row wrap pb-4>
+        <v-flex xs12 xl-1 lg-1 md1 sm12 mt-2>
+          {{ $t("Search by:") }}
+        </v-flex>
+        <v-flex xs12 xl-4 lg-4 md4 sm12 pr-1 mb-1>
+          <v-text-field solo v-model="search" :placeholder="$i18n.t('Name')" single-line hide-details flat>
+          </v-text-field>
+        </v-flex>
+        <v-flex xs12 xl-4 lg-4 md4 sm12 pr-1 mb-1>
+          <v-select
+            solo
+            :items="statusList"
+            item-text="label"
+            item-value="value"
+            v-model="status"
+            hide-details
+            :label="$i18n.t('Status')"
+            :no-data-text="$i18n.t('No data available')"
+          ></v-select>
+        </v-flex>
+        <v-flex xs12 lg-3 xl-3 md3 sm12>
+          <div class="ml-4 pt-3">
+            <a href="#" class="clients-actions">
+              <v-icon>add_circle</v-icon>
+              <router-link :to="{ name: 'NewClient' }" class="blue-color">{{ $t("Add client") }}</router-link>
+            </a>
+          </div>
+        </v-flex>
+      </v-layout>
+      <div v-if="status" class="clients-filters">
+        <v-chip @input="status = null" close>{{ $i18n.t("status") }} : {{ $i18n.t(status) }}</v-chip>
+      </div>
+      <v-data-table
+        :headers="headers"
+        :items="clientsList"
+        :rows-per-page-items="rowsPerPageItems"
+        :pagination.sync="pagination"
+        class="elevation-1"
+        :search="search"
+        :rows-per-page-text="$t('Rows per page:')"
+      >
+        <template slot="items" slot-scope="props">
+          <!-- <td class="text-xs-center">{{ props.item.logo }}</td> -->
+          <td class="text-xs-center">
+            <router-link :to="{ name: 'Client', params: { id: props.item._id } }" class="blue-color">
+              <status-name :name="props.item.name" :active="props.item.active" />
+            </router-link>
+          </td>
+          <td class="text-xs-center">{{ props.item.active ? $t("Yes") : $t("No") }}</td>
+        </template>
+      </v-data-table>
     </div>
-    <span class="clients-search-span hidden-xs-only">{{ $t("Search by:") }}</span>
-    <v-layout row wrap>
-      <v-flex xs6 sm6 md6 lg6 xl6 pr-1 mb-1>
-        <v-text-field v-model="search" :placeholder="$i18n.t('Name')" single-line hide-details solo></v-text-field>
-      </v-flex>
-      <v-flex xs6 sm6 md6 lg6 xl6 mb-1>
-        <v-select
-          solo
-          :items="statusList"
-          item-text="label"
-          item-value="value"
-          v-model="status"
-          hide-details
-          :label="$i18n.t('Status')"
-          :no-data-text="$i18n.t('No data available')"
-        ></v-select>
-      </v-flex>
-    </v-layout>
-    <v-layout justify-end>
-      <v-btn flat :to="{ name: 'NewClient' }">
-        <v-icon class="mr-2">add_circle</v-icon>
-        {{ $t("Add client") }}
-      </v-btn>
-    </v-layout>
-    <div v-if="status" class="clients-filters">
-      <v-chip @input="status = null" close>{{ $i18n.t("status") }} : {{ $i18n.t(status) }}</v-chip>
-    </div>
-    <v-data-table
-      :headers="headers"
-      :items="clientsList"
-      :rows-per-page-items="rowsPerPageItems"
-      :pagination.sync="pagination"
-      class="elevation-1"
-      :search="search"
-      :rows-per-page-text="$t('Rows per page:')"
-    >
-      <template slot="items" slot-scope="props">
-        <!-- <td class="text-xs-center">{{ props.item.logo }}</td> -->
-        <td class="text-xs-center">
-          <router-link :to="{ name: 'Client', params: { id: props.item._id } }" class="blue-color">
-            <status-name :name="props.item.name" :active="props.item.active" />
-          </router-link>
-        </td>
-        <td class="text-xs-center">{{ props.item.active ? $t("Yes") : $t("No") }}</td>
-      </template>
-    </v-data-table>
   </v-container>
 </template>
 
@@ -143,34 +150,6 @@ export default {
   width: 100%;
 }
 
-.clients-search-span {
-  padding-top: 15px;
-  width: 150px;
-  color: #777;
-}
-
-.clients-filters,
-.clients-filters.clients-search {
-  display: inline-flex !important;
-  margin-bottom: 24px;
-}
-
-
-.client-mail {
-  color: #2195f2 !important;
-  font-weight: bold !important;
-}
-
-.clients-operations > a {
-  margin-left: 20px;
-}
-
-.clients-operations {
-  margin: 10px;
-  margin-left: 100px !important;
-  display: flex;
-}
-
 .clients-actions {
   text-decoration: none !important;
   display: flex;
@@ -179,10 +158,6 @@ export default {
 .clients-actions span {
   margin-top: 3.75px;
   margin-left: 2px;
-}
-
-.clients-search {
-  width: 100%;
 }
 
 a.clients-actions:nth-child(1) > a:nth-child(2) {
